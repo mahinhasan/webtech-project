03ee6e8a39c93b3cded23ad00bc77eea65df33f8
mahinhasan <mahinhasanaiub@gmail.com>
2022-11-07 01:00:44 +0600

    blog

---
+++ tourism/blog/blogData.php
@@ -0,0 +1,21 @@
+
+<?php 
+
+//print_r($_FILES);
+
+$src = $_FILES ['myfile']['tmp_name'];
+$des ="blogImage/".$_FILES['myfile']['name'];
+$place = $_POST['place'];
+$date = $_POST['date'];
+$text = $_POST['text'];
+
+$new_post = $place."|".$date."|".$text."\r\n";
+$file = fopen('newblogs.txt', 'a');
+fwrite($file, $new_post);
+if(move_uploaded_file($src, $des)){
+    header('location:home.php');
+}else{
+    echo "Error";
+}
+
+?>
